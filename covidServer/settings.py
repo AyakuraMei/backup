69ff272a1689a6878fff31948869b15eaa847409
Rin <1187128291@qq.com>
2021-02-27 22:37:03 +0800

    update server setting

--- covidServer/settings.py
+++ covidServer/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'covidServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'covid',
+        'USER': 'root',
+        'PASSWORD': '00608620az',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
