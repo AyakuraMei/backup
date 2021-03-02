dc8ee8903da7fb78c2a7b70debddd04caabee956
Rin <1187128291@qq.com>
2021-03-03 01:19:55 +0800

    update code in Login component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
+
+axios.defaults.withCredentials = true
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 // second routes
 export default class App extends Component {
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -36,23 +36,24 @@ export default class Login extends Component {
     sendRegisterMessage = () => {
         const { Username: { state: { value: user } } } = this
         const { Password: { state: { value: pw } } } = this
-        this.setState({ username: user, password: pw })
-        axios.post('/login',
-            {
-                username: this.state.username,
-                password: this.state.password,
-                isRegister: 1,
-            }
-        ).then((response) => {
-            // 收到注册信息后，查看是否存在，如果存在那么进行提示
-            this.state.description = "User already exist."
-            // 如果不存在则进行注册后，那么跳转到 home 界面
-            // 如果成功那么就统一 user 信息
-            this.token = PubSub.publish(USERINFO, { username: this.state.user, password: this.state.password })
-            this.history.push('/home')
-        }).then((error) => {
-                console.log(error)
-        })
+        // 使用 callback function 及时刷新 state 的内容
+        this.setState({ username: user, password: pw }, () => axios.post('http://localhost:8000/login/',
+        {
+            username: this.state.username,
+            password: this.state.password,
+            isRegister: 1,
+        }
+    ).then((response) => {
+        console.log(response)
+        // 收到注册信息后，查看是否存在，如果存在那么进行提示
+        this.state.description = "User already exist."
+        // 如果不存在则进行注册后，那么跳转到 home 界面
+        // 如果成功那么就统一 user 信息
+        this.token = PubSub.publish(USERINFO, { username: this.state.user, password: this.state.password })
+        this.props.history.push('/home/')
+    }).then((error) => {
+            console.log('posted', error)
+    }))
     }
 
     // username 和 password 是用来记录当前发送的用户名和密码
